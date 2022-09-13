e78638e92fd6fcba6c618cf399c99035a8f7d3a6
tratchapong <tratchapong@gmail.com>
2022-09-13 23:31:44 +0700

    Basic filter by category

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,21 @@
 // template from https://mdbootstrap.com/docs/standard/extended/product-cards/
 import axios from 'axios'
 import {useState, useEffect} from 'react'
+import FilterBar from './FilterBar';
 import Product from "./Product";
 
 function Products() {
   const [data, setData] = useState([])
+  const [filter, setFilter] = useState('')
   useEffect( ()=>{
-    axios.get('http://localhost:8080/products').then(res => {
+    axios.get('http://localhost:8080/products' + filter).then(res => {
       setData(res.data)
     })
-  },[])
+  },[filter])
 
   return (
     <section style={{ backgroundColor: "#eee" }}>
+      <FilterBar setFilter={setFilter} />
       <div className="container py-5">
         { data.map( item => (
           <Product key={item.id} item={item} />

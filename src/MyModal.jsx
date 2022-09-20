84c65be91c7f9c3fa45db6c516b2f57321fe04ec
tratchapong <tratchapong@gmail.com>
2022-09-21 01:11:24 +0700

    Modal with transition

--- src/MyModal.jsx
+++ src/MyModal.jsx
@@ -10,20 +10,27 @@ function MyModal(props) {
 
   return (
     <div
-      className={`relative z-10 ${open ? "" : "hidden"}`}
+      className={`relative z-10`}
+      // className={`relative z-10 ${open ? "opacity-100" : "opacity-0"}`}
+      // className={`relative z-10 ${open ? "" : "hidden"}`}
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
     >
       <div
-        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-        
+        className={`fixed ${open ? "inset-0" : ""} bg-gray-500 opacity-75 `}
       ></div>
 
-      <div className="fixed inset-0 z-10 overflow-y-auto" onClick={closeModal}  >
+      <div
+        className={`fixed ${open ? "inset-0" : ""} z-10 overflow-y-auto`}
+        onClick={closeModal}
+      >
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg" 
-            onClick={e=>e.stopPropagation()}
+          <div
+            className={`relative ${
+              open ? "scale-100" : "scale-0"
+            } overflow-hidden rounded-lg bg-white text-left shadow-xl transition-transform duration-300 sm:my-8 sm:w-full sm:max-w-lg`}
+            onClick={(e) => e.stopPropagation()}
           >
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">

fa5603692373fc91187a1485a1cb383edbf68393
tratchapong <tratchapong@gmail.com>
2022-09-23 23:37:30 +0700

    create TwModal with pure tailwind from scratch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginForm from "./LoginForm";
 import MyModal from "./MyModal";
 import Navbar from "./Navbar";
 import ProductList from "./ProductList";
+import TwModal from "./TwModal";
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -13,15 +14,17 @@ function App() {
       {/* <ChitChat />
       <Accordian /> */}
       <Navbar />
-      <button className="my-3 mx-auto block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        onClick={()=>setOpen(!open)}
+      <button
+        className="my-3 mx-auto block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        onClick={() => setOpen(!open)}
       >
         Open Modal
       </button>
-      <LoginForm />
+      {/* <LoginForm />
       <ProductList />
 
-      <MyModal open={open} setOpen={setOpen} />
+      <MyModal open={open} setOpen={setOpen} /> */}
+      <TwModal />
     </div>
   );
 }
